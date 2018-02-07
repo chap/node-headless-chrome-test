c9b455e825aa16f1d0731adc4ae46b9de9133544
Chap Ambrose <chapambrose@gmail.com>
2018-02-07 13:30:51 -0600

    test script should start server

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
+var myApp = require("./index.js")
 const puppeteer = require('puppeteer');
 const test = require('tape');
 const PORT = process.env.PORT || 5000
@@ -6,12 +7,13 @@ test('responds to requests', (t) => {
   t.plan(1);
 
   (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     const response = await page.goto('http://0.0.0.0:'+PORT);
 
     t.equal(response.status(), 200);
 
     await browser.close();
+    myApp.closeServer();
   })();
 });
