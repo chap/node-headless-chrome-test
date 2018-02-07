2819f1ca15ade32a7f625f150f9415033b96d447
Chap Ambrose <chapambrose@gmail.com>
2018-02-07 12:51:13 -0600

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ test('responds to requests', (t) => {
   t.plan(1);
 
   (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     const response = await page.goto('http://0.0.0.0:'+PORT);
 
